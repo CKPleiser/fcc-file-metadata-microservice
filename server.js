f299198200b522ddee238e4a75d35b1c622bc039
Your Real Name Here <Your Email Address Here>
2019-09-02 22:00:12 +0100

    Ready

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage, limits: {fileSize: 300000000}})
+// require and use "multer"...
+
+const app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', (req, res) => {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const file = req.file;
+  if (!file) {
+    const error = new Error('Please upload a file');
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  res.send({ name: file.originalname, type: file.mimetype, size: file.size })
+})
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Node.js listening ...');
+});
